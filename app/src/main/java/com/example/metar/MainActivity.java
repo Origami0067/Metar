b16198f24557167038abddba2cbaa41ac6e72c97
Origami0067 <volpatogerard@yahoo.com>
2021-10-22 11:32:06 +0200

    update

--- app/src/main/java/com/example/metar/MainActivity.java
+++ app/src/main/java/com/example/metar/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.metar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -15,10 +21,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ParseAdapter adapter;
+    private ArrayList<ParseItem> parseItems = new ArrayList<>();
+    private ProgressBar progressBar;
 
     private Button getBtn;
     private TextView resultat;
@@ -45,6 +55,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ParseAdapter(parseItems, this);
+        recyclerView.setAdapter(adapter);
+
+        Content content = new Content();
+        content.execute();
 
         //get instances of the Button and the TextView from our layout
         resultat = (TextView)findViewById(R.id.resultat);
@@ -107,4 +128,64 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    private class Content extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressBar.setVisibility(View.GONE);
+            progressBar.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                String url = "https://www.cinemaqatar.com/";
+
+                Document doc = Jsoup.connect(url).get();
+
+                Elements data = doc.select("span.thumbnail");
+                int size = data.size();
+                Log.d("doc", "doc: "+doc);
+                Log.d("data", "data: "+data);
+                Log.d("size", ""+size);
+                for (int i = 0; i < size; i++) {
+                    String imgUrl = data.select("span.thumbnail")
+                            .select("img")
+                            .eq(i)
+                            .attr("src");
+
+                    String title = data.select("h4.gridminfotitle")
+                            .select("span")
+                            .eq(i)
+                            .text();
+
+                    String detailUrl = data.select("h4.gridminfotitle")
+                            .select("a")
+                            .eq(i)
+                            .attr("href");
+
+                    parseItems.add(new ParseItem(imgUrl, title, detailUrl));
+                    Log.d("items", "img: " + imgUrl + " . title: " + title);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+    }
+}
 }
